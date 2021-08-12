c4b0caa556358d5ca579053222a6ab44597a9dde
Nate <nfoster@ranker.com>
2021-08-12 13:09:41 -0700

    Service Worker

--- src/serviceWorkerConfig.js
+++ src/serviceWorkerConfig.js
@@ -32,47 +32,42 @@ const receivePushNotification = (event, registration) => {
   event.waitUntil(registration.showNotification(title, options));
 };
 
-const openPushNotification = event => {
-  console.log(
-    '[Service Worker] Notification click Received.',
-    event.notification.data
-  );
+const openPushNotification = (event) => {
+  console.log('[Service Worker] Notification click Received.', event.notification.data);
 
   event.notification.close();
   // event.waitUntil(clients.openWindow(event.notification.data))
 };
 
-const config = store => ({
+const config = (store) => ({
   // onUpdate: (registration) => {
   //   registration.unregister().then(() => {
   //     window.location.reload()
   //   })
   // },
-  onUpdate: registration => {
+  onUpdate: (registration) => {
     const waitingServiceWorker = registration.waiting;
 
     if (waitingServiceWorker) {
-      waitingServiceWorker.addEventListener('statechange', event => {
+      waitingServiceWorker.addEventListener('statechange', (event) => {
         if (event.target.state === 'activated') {
+          alert('Update Available! Please refresh your browser.');
           if (store) {
           } else {
-            alert('Update Available! Please refresh your browser.');
+            window.location.reload();
           }
         }
       });
       waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
     }
   },
-  onSuccess: registration => {
+  onSuccess: (registration) => {
     console.info('Service worker on success state');
     console.log(registration);
 
-    window.addEventListener('push', e =>
-      receivePushNotification(e, registration)
-    );
+    window.addEventListener('push', (e) => receivePushNotification(e, registration));
     window.addEventListener('notificationclick', openPushNotification);
-  },
-  store
+  }
 });
 
 export default config;
