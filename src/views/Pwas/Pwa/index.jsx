7f8bb73f7cb5a50113ce9ee944d0d2aff305e0dd
Nate <nfoster@ranker.com>
2021-08-13 15:15:17 -0700

    Card links

--- src/views/Pwas/Pwa/index.jsx
+++ src/views/Pwas/Pwa/index.jsx
@@ -1,19 +1,19 @@
 import React, { memo } from 'react';
 import Tags from './Tags';
+import { Link } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { CardActionArea } from '@material-ui/core';
 import { useBooleanReducer } from 'resurrection';
-import { useHistory } from 'react-router-dom';
 import { GetPwaDetailUrl } from 'utils/RouteMap';
 
 const DEFAULT_IMAGE = 'https://gpndata.com/blog/wp-content/uploads/2016/09/Cover1-1024x1024.jpg';
 
 const IMAGE_SIZE = 140;
 
-const cardStyles = { textAlign: 'center' };
+const cardStyles = { textAlign: 'center', boxShadow: 'none', textDecoration: 'none' };
 
 const noWrapStyles = {
   whiteSpace: 'nowrap',
@@ -21,6 +21,8 @@ const noWrapStyles = {
   overflow: 'hidden',
   maxWidth: '100%'
 };
+
+const nameStyles = { ...noWrapStyles, textDecoration: 'underline' };
 
 const Pwa = ({
   id,
@@ -34,26 +36,25 @@ const Pwa = ({
   tags,
   last_modified
 }) => {
-  const history = useHistory();
-  const [isHovered, toggleIsHovered] = useBooleanReducer(false);
+  // const [isHovered, toggleIsHovered] = useBooleanReducer(false);
 
-  const handleCardClick = () => {
-    history.push(GetPwaDetailUrl(id));
-  };
+  const pwaRoute = GetPwaDetailUrl(id);
 
   return (
     <Card
-      raised={isHovered}
+      component={Link}
+      to={pwaRoute}
+      // variant='outlined'
+      // raised={isHovered}
       sx={{ width: IMAGE_SIZE }}
-      onMouseEnter={toggleIsHovered}
-      onMouseLeave={toggleIsHovered}
+      // onMouseEnter={toggleIsHovered}
+      // onMouseLeave={toggleIsHovered}
       style={cardStyles}
-      onClick={handleCardClick}
     >
       <CardActionArea>
         <CardMedia sx={{ height: IMAGE_SIZE, width: IMAGE_SIZE }} image={icon_url || DEFAULT_IMAGE} title={name} />
         <CardContent>
-          <Typography gutterBottom variant='span' component='div' style={noWrapStyles}>
+          <Typography gutterBottom variant='span' component='div' style={nameStyles}>
             {name}
           </Typography>
           <Typography variant='body3' color='text.secondary' style={noWrapStyles}>
