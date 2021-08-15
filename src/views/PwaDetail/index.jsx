0514537ed05595b715b9a00486885373275ae897
Nathan Foster <nateinthegame@gmail.com>
2021-08-14 19:00:05 -0700

    Launch button

--- src/views/PwaDetail/index.jsx
+++ src/views/PwaDetail/index.jsx
@@ -1,16 +1,19 @@
 import React, { useMemo } from 'react';
+import PropTypes from 'prop-types';
 import connect from 'store/connect';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Stack from '@material-ui/core/Stack';
+
 
 const PwaDetail = ({
   id,
   name,
   description,
   pwa_analytics: { view_count, launch_count },
-
   ratings,
   organization,
   tags,
@@ -33,16 +36,19 @@ const PwaDetail = ({
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={6}>
+      <Grid item xs={12}>
         <Typography variant='h3'>{name}</Typography>
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={3}>
+        <Button variant='contained' disabled={!url} href={url} target='_blank'>Launch App</Button>
+      </Grid>
+      <Grid item xs={3}>
         <Typography variant='h6'>Views: {view_count}</Typography>
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={3}>
         <Typography variant='h6'>Launches: {launch_count}</Typography>
       </Grid>
-      <Grid item xs={2}>
+      <Grid item xs={3}>
         <Typography variant='h6'>Average Rating: {averageRating}</Typography>
       </Grid>
       <Grid item xs={12}>
