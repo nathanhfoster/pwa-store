6ed9dad1586fd718d49f51333e91a5cebe202b53
Nathan Foster <nateinthegame@gmail.com>
2021-08-17 01:13:35 -0700

    Update index.jsx

--- src/components/LoadingScreen/index.jsx
+++ src/components/LoadingScreen/index.jsx
@@ -7,8 +7,9 @@ cont containerStyles = {
    right: 0,
    display: 'flex',
    justifyContent: 'center',
-   alignContent: 'center'
-};
+   alignContent: 'center',
+   backgroundImage: 'radial-gradient(circle, #004aad, #0059b8, #0068c1, #0077ca, #1785d2, #218ed8, #2b96de, #359fe4, #36a5eb, #37aaf1, #37b0f8, #38b6ff)'
+}
 
 const imageStyles = {
    maxHeight: '100vh',
