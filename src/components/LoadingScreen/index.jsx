1df27d07581156ae26b0f689aeb06a8079b45210
Nathan Foster <nateinthegame@gmail.com>
2021-08-17 01:10:55 -0700

    Update index.jsx

--- src/components/LoadingScreen/index.jsx
+++ src/components/LoadingScreen/index.jsx
@@ -1,3 +1,22 @@
-const LoadingScreen = () => <div>Loading...</div>;
+cont containerStyles = {
+   zIndex: 9999,
+   position: 'absolute',
+   top: 0,
+   bottom: 0,
+   left: 0,
+   right: 0,
+   display: 'flex',
+   justifyContent: 'center',
+   alignContent: 'center'
+};
+
+const imageStyles = {
+   maxHeight: '100vh',
+   maxWidth: '100vw'
+};
+
+const AppIcon = `${process.env.PUBLIC_URL}/assets/android-chrome-512x512.png`;
+
+const LoadingScreen = () => <div style={containerStyles}><img src={AppIcon} style={imageStyles} /></div>;
 
 export default LoadingScreen;
