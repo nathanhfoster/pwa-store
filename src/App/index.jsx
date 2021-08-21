2a02302c6dcce9ef01cc8c33b4b9fa666668c4a1
Nathan Foster <nateinthegame@gmail.com>
2021-08-20 18:59:50 -0700

    Update index.jsx

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -67,9 +67,7 @@ const App = ({ GetUserSettings, GetPwas, SetAddToHomeScreenPrompt, GetPwaTags, U
             mt: { xs: '56px', sm: '64px' }
           }}
         >
-          <Box component='header' sx={{ overflowY: 'auto' }}>
-            <AppRouter />
-          </Box>
+          <AppRouter />
         </Box>
       </Container>
     </ThemeProvider>
