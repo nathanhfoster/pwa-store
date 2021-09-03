d1ad2d139d007e9ccc205e989c153fa8d874cd90
Nathan Foster <nateinthegame@gmail.com>
2021-09-02 20:30:06 -0700

    Slugify

--- src/utils/index.js
+++ src/utils/index.js
@@ -154,3 +154,14 @@ export const isEmpty = (obj) => {
 };
 
 export const joinUrl = (baseUrl, url) => new URL(url, baseUrl).href;
+
+export const slugify = text =>
+  text
+    .toString()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-');
