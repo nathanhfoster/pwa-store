47b9c63897d6f35c479c39dc14017460bb6d11fb
Nathan Foster <nateinthegame@gmail.com>
2021-09-03 18:45:42 -0700

    Update utils.js

--- src/store/reducers/App/utils.js
+++ src/store/reducers/App/utils.js
@@ -1,3 +1,3 @@
-export const DEFAULT_ALERT_TIMEOUT = 3000;
+export const DEFAULT_ALERT_TIMEOUT = 1800;
 export const BASE_ALERT_ID = 'Alert';
 export const getReduxAlertId = () => `${BASE_ALERT_ID}${new Date().getTime()}`;
