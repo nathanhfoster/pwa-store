fbc7dc8cc2c0ee39a03a46c95c322b7c4670a5e8
Nathan Foster <nfoster@rnaker.com>
2021-09-04 19:12:26 -0700

    List

--- src/App/NavDrawer/NavList/index.jsx
+++ src/App/NavDrawer/NavList/index.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, lazy } from 'react';
+import React, { lazy, memo } from 'react';
 import PropTypes from 'prop-types';
 import connect from 'resurrection';
 import { ToggleAppNavBar } from 'store/reducers/App/actions';
@@ -8,12 +8,23 @@ import { useHistory } from 'react-router-dom';
 import { HOME, SETTINGS_USER_FAVORITE_PWAS } from 'utils/RouteMap';
 import Toolbar from '@material-ui/core/Toolbar';
 import Divider from '@material-ui/core/Divider';
-import List from '@material-ui/core/List';
 import IconButton from '@material-ui/core/IconButton';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import HomeIcon from '@material-ui/icons/StoreMallDirectory';
+import { FixedSizeList as List, areEqual } from 'react-window';
+import { APP_DRAWER_WIDTH, APP_DRAWER_HEIGHT } from '../../../constants';
 
 const NavItem = lazy(() => import('./NavItem'));
+
+const Row = memo(({ index, style, data }) => {
+  const tag = data[index];
+  if (!tag) return null;
+  return (
+    <div style={style}>
+      <NavItem key={tag.name} {...tag} />
+    </div>
+  );
+}, areEqual);
 
 const StyledToolbar = styled(Toolbar)((props) => ({
   display: 'flex',
@@ -30,7 +41,7 @@ const iconStyles = {
   '&:hover': { color: 'primary.main' }
 };
 
-const NavList = ({ tags, ResetPwasFilter, ToggleAppNavBar }) => {
+const NavList = ({ height, tags, ResetPwasFilter, ToggleAppNavBar }) => {
   const history = useHistory();
 
   const handleResetNavBar = () => {
@@ -48,8 +59,6 @@ const NavList = ({ tags, ResetPwasFilter, ToggleAppNavBar }) => {
     handleResetNavBar();
   };
 
-  const renderTags = useMemo(() => tags.map((tag) => <NavItem key={tag.name} {...tag} />), [tags]);
-
   return (
     <>
       <StyledToolbar>
@@ -61,12 +70,17 @@ const NavList = ({ tags, ResetPwasFilter, ToggleAppNavBar }) => {
         </IconButton>
       </StyledToolbar>
       <Divider />
-      <List>{renderTags}</List>
+      <List width={APP_DRAWER_WIDTH} height={height} itemCount={tags.length} itemData={tags} itemSize={48}>
+        {Row}
+      </List>
       <Divider />
     </>
   );
 };
-const mapStateToProps = ({ Pwas: { tags } }) => ({ tags });
+const mapStateToProps = ({ Pwas: { tags }, Window: { innerHeight } }) => ({
+  height: innerHeight - APP_DRAWER_HEIGHT,
+  tags
+});
 const mapDispatchToProps = { ResetPwasFilter, ToggleAppNavBar };
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavList);
