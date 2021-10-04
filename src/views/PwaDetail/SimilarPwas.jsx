f26444e1dac7b7e2a3533c9e8ba25bbdf7fa393f
Nathan Foster <nfoster@rnaker.com>
2021-10-03 21:30:47 -0700

    Loadmore

--- src/views/PwaDetail/SimilarPwas.jsx
+++ src/views/PwaDetail/SimilarPwas.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { PwaTagType } from 'store/reducers/Pwas/types';
 import { PwasStack } from 'components';
 import connect from 'resurrection';
+import { GetPwas } from 'store/reducers/Pwas/actions/api';
 
-const SimilarPwas = ({ pwas, tags }) => {
-  return <PwasStack title='Similar Pwas' data={pwas} />;
+const SimilarPwas = ({ pwas, next, tags, GetPwas }) => {
+  const loadMoreData = useCallback(() => {
+    if (next) {
+      GetPwas(next);
+    }
+  }, [next]);
+  return <PwasStack title='Similar Pwas' data={pwas} loadMoreData={loadMoreData} />;
 };
 
 SimilarPwas.propTypes = { tags: PropTypes.arrayOf(PropTypes.shape(PwaTagType)) };
 
 SimilarPwas.defaultProps = { tags: [] };
 
-const mapStateToProps = ({ Pwas: { items, filteredItems } }, { tags }) => ({
+const mapStateToProps = ({ Pwas: { items, filteredItems, next } }, { tags }) => ({
   pwas: items.concat(filteredItems).filter((pwa) => pwa?.tags.some(({ name }) => tags.some((tag) => tag.name === name)))
 });
 
-export default connect(mapStateToProps)(SimilarPwas);
+const mapDispatchToProps = { GetPwas };
+
+export default connect(mapStateToProps, mapDispatchToProps)(SimilarPwas);
