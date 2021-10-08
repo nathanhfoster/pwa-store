3be3a7e340122deac253c47bd4ded2b631468f8d
Nathan Foster <nfoster@rnaker.com>
2021-10-08 12:27:49 -0700

    Better

--- src/views/PwaDetail/SimilarPwas.jsx
+++ src/views/PwaDetail/SimilarPwas.jsx
@@ -1,12 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { PwaTagType } from 'store/reducers/Pwas/types';
 import { PwasStack } from 'components';
 import connect from 'resurrection';
 import { SearchPwas } from 'store/reducers/Pwas/actions/api';
 
-const SimilarPwas = ({ pwas, tags, SearchPwas }) => {
-  const tag = tags?.length > 0 ? tags[0].name : '';
+const SimilarPwas = ({ pwas, next, tags, SearchPwas }) => {
+  const [tagIndex, setTagIndex] = useState(0);
+
+  const tag = tags[tagIndex].name;
+
+  useEffect(() => {
+    if (!next) {
+      setTagIndex((index) => {
+        if (tags?.length > index + 1) {
+          return index + 1;
+        }
+        return index;
+      });
+    }
+  }, [next, tags?.length]);
 
   useEffect(() => {
     SearchPwas(tag);
@@ -19,11 +32,23 @@ SimilarPwas.propTypes = { tags: PropTypes.arrayOf(PropTypes.shape(PwaTagType)) }
 
 SimilarPwas.defaultProps = { tags: [] };
 
-const mapStateToProps = ({ Pwas: { items, filteredItems } }, { pwaSlug, tags }) => ({
-  pwas: items
-    .concat(filteredItems)
-    .filter((pwa) => pwa.slug !== pwaSlug && pwa.tags.some(({ name }) => tags.some((tag) => tag.name === name)))
-});
+const mapStateToProps = (
+  {
+    Pwas: {
+      items,
+      filteredItems,
+      search: { next }
+    }
+  },
+  { pwaSlug, tags }
+) => {
+  return {
+    next,
+    pwas: items
+      .concat(filteredItems)
+      .filter((pwa) => pwa.slug !== pwaSlug && pwa.tags.some(({ name }) => tags.some((tag) => tag.name === name)))
+  };
+};
 
 const mapDispatchToProps = { SearchPwas };
 
