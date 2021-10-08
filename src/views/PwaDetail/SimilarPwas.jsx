53ec0e70167f324cfdd820d952c9998618e8e3bf
Nathan Foster <nfoster@rnaker.com>
2021-10-08 14:21:05 -0700

    Optimized

--- src/views/PwaDetail/SimilarPwas.jsx
+++ src/views/PwaDetail/SimilarPwas.jsx
@@ -1,22 +1,30 @@
-import React, { useRef, useLayoutEffect, useEffect } from 'react';
+import React, { useRef, useMemo, useLayoutEffect, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { PwaTagType } from 'store/reducers/Pwas/types';
 import { PwasStack } from 'components';
 import connect from 'resurrection';
 import { SearchPwas } from 'store/reducers/Pwas/actions/api';
 
-const SimilarPwas = ({ pwas, next, pwaSlug, tags, SearchPwas }) => {
+const SimilarPwas = ({ next, items, filteredItems, pwaSlug, tags, SearchPwas }) => {
   const tagIndex = useRef(0);
+
+  const pwas = useMemo(
+    () =>
+      items
+        .concat(filteredItems)
+        .filter((pwa) => pwa.slug !== pwaSlug && pwa.tags.some(({ name }) => tags.some((tag) => tag.name === name))),
+    [filteredItems, items, pwaSlug, tags]
+  );
 
   const tag = tags[tagIndex.current].name;
 
   useLayoutEffect(() => {
     tagIndex.current = 0;
-  }, [pwaSlug])
+  }, [pwaSlug]);
 
   useLayoutEffect(() => {
     if (!next && tags?.length > tagIndex.current + 1) {
-       tagIndex.current++;
+      tagIndex.current++;
     }
   }, [next, tags?.length]);
 
@@ -31,23 +39,17 @@ SimilarPwas.propTypes = { tags: PropTypes.arrayOf(PropTypes.shape(PwaTagType)) }
 
 SimilarPwas.defaultProps = { tags: [] };
 
-const mapStateToProps = (
-  {
-    Pwas: {
-      items,
-      filteredItems,
-      search: { next }
-    }
-  },
-  { pwaSlug, tags }
-) => {
-  return {
-    next,
-    pwas: items
-      .concat(filteredItems)
-      .filter((pwa) => pwa.slug !== pwaSlug && pwa.tags.some(({ name }) => tags.some((tag) => tag.name === name)))
-  };
-};
+const mapStateToProps = ({
+  Pwas: {
+    items,
+    filteredItems,
+    search: { next }
+  }
+}) => ({
+  next,
+  items,
+  filteredItems
+});
 
 const mapDispatchToProps = { SearchPwas };
 
