a30d8de7bf9b43b627b5b42cdf57efae0573ccf9
Nathan Foster <nateinthegame@gmail.com>
2021-10-07 18:34:55 -0700

    Update SimilarPwas.jsx

--- src/views/PwaDetail/SimilarPwas.jsx
+++ src/views/PwaDetail/SimilarPwas.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { PwaTagType } from 'store/reducers/Pwas/types';
 import { PwasStack } from 'components';
 import connect from 'resurrection';
 import { SearchPwas } from 'store/reducers/Pwas/actions/api';
 
-const SimilarPwas = ({ pwas, SearchPwas }) => {
-  return <PwasStack title='Similar Pwas' data={pwas} loadMoreData={SearchPwas} />;
+const SimilarPwas = ({ pwas, tags, SearchPwas }) => {
+  const tag = tags.length > 0 ? tags[0].name || '';
+  const loadMoreData = useCallback(() => {
+     if(tag) {
+       SearchPwas(tag);
+     }
+  }, [tag])
+  return <PwasStack title='Similar Pwas' data={pwas} loadMoreData={loadMoreData} />;
 };
 
 SimilarPwas.propTypes = { tags: PropTypes.arrayOf(PropTypes.shape(PwaTagType)) };
